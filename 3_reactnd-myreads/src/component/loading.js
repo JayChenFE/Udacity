402589ad5a15557ff7e398fc3459697358e456af
JayChenFE <chenjiehwy@163.com>
2017-09-30 11:47:29 +0800

    igone

--- 3_reactnd-myreads/src/component/loading.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactLoading from 'react-loading'
-
-const Loading = ({ type, color }) => (
-    <ReactLoading type={type} color={color} style={{ height: 800, width: 200, margin: '0 auto' }} />
-)
-
-export default Loading 
